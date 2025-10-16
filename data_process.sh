eb70f9dd280d9024bd2b5a13d0497e9bdc8e024f
masakifujiwara1 <fmasaki2020@gmail.com>
2025-10-16 13:04:39 +0900

    Update data_process.sh with correct paths and reduce total scenarios

--- data_process.sh
+++ data_process.sh
@@ -1,13 +1,13 @@
 ###################################
 # User Configuration Section
 ###################################
-NUPLAN_DATA_PATH="REPLACE_WITH_NUPLAN_DATA_PATH" # nuplan training data path (e.g., "/data/nuplan-v1.1/trainval")
-NUPLAN_MAP_PATH="REPLACE_WITH_NUPLAN_MAP_PATH" # nuplan map path (e.g., "/data/nuplan-v1.1/maps")
+NUPLAN_DATA_PATH="/home/ubuntu/nuplan/dataset/nuplan-v1.1/splits/mini" # nuplan training data path (e.g., "/data/nuplan-v1.1/trainval")
+NUPLAN_MAP_PATH="/home/ubuntu/nuplan/dataset/maps" # nuplan map path (e.g., "/data/nuplan-v1.1/maps")
 
-TRAIN_SET_PATH="REPLACE_WITH_TRAIN_SET_PATH" # preprocess training data
+TRAIN_SET_PATH="/home/ubuntu/nuplan/preprocess" # preprocess training data
 ###################################
 
-python data_process.py \
+python3 data_process.py \
 --data_path $NUPLAN_DATA_PATH \
 --map_path $NUPLAN_MAP_PATH \
 --save_path $TRAIN_SET_PATH \
